750abd4564817fe733ca633cf1bcca2be1c6476b
dell <hendaoye.201250@gamil.com>
2025-01-26 16:35:21 +0200

    commit : single page

--- wp-content/themes/threeSixty_theme/partials/recent-card.php
+++ wp-content/themes/threeSixty_theme/partials/recent-card.php
@@ -32,9 +32,32 @@ $thumbnail_id = get_post_thumbnail_id($post_id);
     <svg class="post-title-svg" width="24" height="28" viewBox="0 0 24 28" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M7 21L17 11M17 11H7M17 11V21" stroke="#CA8504" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>
-
   </a>
   <?php if ($post_excerpt): ?>
     <span class="excerpt text-md gray-500"><?= esc_html($post_excerpt) ?></span>
   <?php endif; ?>
+  <div class="categories">
+    <?php
+    $categories = get_the_category();
+    if ($categories) {
+      foreach ($categories as $category) {
+        $text_color = get_field('text_color', 'category_' . $category->term_id);
+        $background_color = get_field('background_color', 'category_' . $category->term_id);
+        $border_color = get_field('border_color', 'category_' . $category->term_id);
+
+        // Set default colors if ACF fields are empty
+        $text_color = $text_color ? esc_attr($text_color) : '#A15C07';
+        $background_color = $background_color ? esc_attr($background_color) : '#FEFBE8';
+        $border_color = $border_color ? esc_attr($border_color) : '#FDE172';
+
+        echo '<div class="cat-name text-sm medium"
+                    style="color: ' . $text_color . ';
+                           background-color: ' . $background_color . ';
+                           border-color: ' . $border_color . ';">
+                    ' . esc_html($category->name) . '
+                  </div>';
+      }
+    }
+    ?>
+  </div>
 </div>
--- wp-content/themes/threeSixty_theme/single.php
+++ wp-content/themes/threeSixty_theme/single.php
@@ -1,5 +1,14 @@
 <?php
 get_header();
+global $post;
+$post_id = get_the_ID();
+$author_id = $post->post_author;
+$author_name = get_the_author_meta('display_name', $author_id);
+$author_jop = get_the_author_meta('display_name', $author_id);
+$author_image = get_the_author_meta('display_name', $author_id);
+$current_user_id = get_current_user_id();
+$user_image = get_field('user_image', 'user_' . $current_user_id);
+$user_jop_title = get_field('user_jop_title', 'user_' . $current_user_id);
 ?>
 <?php if (have_posts()): the_post(); ?>
   <div class="single-post-wrapper">
@@ -49,9 +58,7 @@ get_header();
           <div class="dynamic-content"  >
             <?php the_content(); ?>
           </div>
-
           <!--     add new code here -->
-
           <!--           tages -->
             <?php
             $tags = get_the_tags();
@@ -65,18 +72,19 @@ get_header();
               echo '</div>';
             }
             ?>
-
 <!--           bootm content -->
 <!-- authot -->
           <div class="author-social-links">
             <div class="about-author">
-              <picture class="image-author">
-                <img src=" <?= get_template_directory_uri() . '/images/backgrounds/author-image.png' ?>" alt="trustpilot">
-              </picture>
+
+              <?php if (!empty($user_image) && is_array($user_image)) { ?>
+                <picture class="image-author">
+                  <img src="<?= $user_image['url'] ?>" alt="<?= $user_image['alt'] ?>">
+                </picture>
+              <?php } ?>
               <div class="author-info">
-                <h5 class="text-sm semi-bold author-name">Dr. Muneer Hamed</h5>
-                <h6 class="text-sm gray-600 author-jop">Senior Marketing
-                  Consultant</h6>
+                <h5 class="text-sm semi-bold author-name"><?= $author_name ?></h5>
+                <h6 class="text-sm gray-600 author-jop"> <?= $user_jop_title ?></h6>
               </div>
             </div>
             <div class="social-links">
@@ -93,7 +101,7 @@ get_header();
                 </svg>
                 Copy link
               </a>
-              <a class="social-link" href="#">
+              <a class="social-link" href="https://twitter.com/intent/tweet?url=<?php echo urlencode(get_permalink()); ?>&text=<?php echo urlencode(get_the_title()); ?>" target="_blank">
                 <svg width="20" height="20" viewBox="0 0 20 20" fill="none" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
                   <g clip-path="url(#clip0_273_4048)">
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M13.2879 19.1668L8.66337 12.5752L2.87405 19.1668H0.424805L7.57674 11.0261L0.424805 0.833496H6.71309L11.0717 7.04601L16.5327 0.833496H18.982L12.1619 8.59723L19.5762 19.1668H13.2879ZM16.0154 17.3085H14.3665L3.93176 2.69183H5.58092L9.7601 8.54446L10.4828 9.56005L16.0154 17.3085Z" fill="#9AA3B2"/>
@@ -105,7 +113,7 @@ get_header();
                   </defs>
                 </svg>
               </a>
-              <a class="social-link" href="#">
+              <a class="social-link" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode(get_permalink()); ?>" target="_blank">
                 <svg width="20" height="20" viewBox="0 0 20 20" fill="none" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
                   <g clip-path="url(#clip0_273_4051)">
                     <path d="M20 10C20 4.47715 15.5229 0 10 0C4.47715 0 0 4.47715 0 10C0 14.9912 3.65684 19.1283 8.4375 19.8785V12.8906H5.89844V10H8.4375V7.79688C8.4375 5.29063 9.93047 3.90625 12.2146 3.90625C13.3084 3.90625 14.4531 4.10156 14.4531 4.10156V6.5625H13.1922C11.95 6.5625 11.5625 7.3334 11.5625 8.125V10H14.3359L13.8926 12.8906H11.5625V19.8785C16.3432 19.1283 20 14.9912 20 10Z" fill="#9AA3B2"/>
@@ -118,7 +126,7 @@ get_header();
                   </defs>
                 </svg>
               </a>
-              <a class="social-link" href="#">
+              <a class="social-link" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo urlencode(get_permalink()); ?>&title=<?php echo urlencode(get_the_title()); ?>" target="_blank">
                 <svg width="20" height="20" viewBox="0 0 20 20" fill="none" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
                   <g clip-path="url(#clip0_273_4055)">
                     <path d="M18.5236 0H1.47639C1.08483 0 0.709301 0.155548 0.432425 0.432425C0.155548 0.709301 0 1.08483 0 1.47639V18.5236C0 18.9152 0.155548 19.2907 0.432425 19.5676C0.709301 19.8445 1.08483 20 1.47639 20H18.5236C18.9152 20 19.2907 19.8445 19.5676 19.5676C19.8445 19.2907 20 18.9152 20 18.5236V1.47639C20 1.08483 19.8445 0.709301 19.5676 0.432425C19.2907 0.155548 18.9152 0 18.5236 0ZM5.96111 17.0375H2.95417V7.48611H5.96111V17.0375ZM4.45556 6.1625C4.11447 6.16058 3.7816 6.05766 3.49895 5.86674C3.21629 5.67582 2.99653 5.40544 2.8674 5.08974C2.73826 4.77404 2.70554 4.42716 2.77336 4.09288C2.84118 3.7586 3.0065 3.4519 3.24846 3.21148C3.49042 2.97107 3.79818 2.80772 4.13289 2.74205C4.4676 2.67638 4.81426 2.71133 5.12913 2.84249C5.44399 2.97365 5.71295 3.19514 5.90205 3.47901C6.09116 3.76288 6.19194 4.09641 6.19167 4.4375C6.19488 4.66586 6.15209 4.89253 6.06584 5.104C5.97959 5.31547 5.85165 5.50742 5.68964 5.66839C5.52763 5.82936 5.33487 5.95607 5.12285 6.04096C4.91083 6.12585 4.68389 6.16718 4.45556 6.1625ZM17.0444 17.0458H14.0389V11.8278C14.0389 10.2889 13.3847 9.81389 12.5403 9.81389C11.6486 9.81389 10.7736 10.4861 10.7736 11.8667V17.0458H7.76667V7.49306H10.6583V8.81667H10.6972C10.9875 8.22917 12.0042 7.225 13.5556 7.225C15.2333 7.225 17.0458 8.22083 17.0458 11.1375L17.0444 17.0458Z" fill="#9AA3B2"/>
@@ -133,16 +141,54 @@ get_header();
               </a>
             </div>
           </div>
-
         </div>
-
-
       </div>
     </div>
 
 
 <!--     recent posts -->
-
+    <section id="recent_posts" data-section-class="recent_posts" class="threeSixty_theme-block recent_posts js-loaded">
+      <div class="container">
+        <h5 class="semi-bold recent-content-title">Related Posts</h5>
+        <?php
+        $current_post_id = get_the_ID();
+        $args = [
+          'post_type' => 'post',
+          'posts_per_page' => 3,
+          'post__not_in' => [$current_post_id],
+          'orderby' => 'date',
+          'order' => 'DESC',
+        ];
+        $query = new WP_Query($args);
+        ?>
+        <?php if ($query->have_posts()): ?>
+          <div class="swiper recent-posts-swiper">
+            <div class="swiper-wrapper">
+              <?php while ($query->have_posts()): $query->the_post(); ?>
+                <?php
+                get_template_part("partials/recent-card", "", ["post_id" => get_the_ID()]);
+                ?>
+              <?php endwhile; ?>
+            </div>
+          </div>
+          <?php wp_reset_postdata(); ?>
+        <?php endif; ?>
+        <div class="swiper-navigations">
+          <div class="swiper-button-prev swiper-navigation arrow animation-scale-me" role="button" tabindex="0" aria-label="Previous Slide">
+            <svg width="56" height="56" viewBox="0 0 56 56" fill="none" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
+              <path d="M0.5 0.5H55.5V55.5H0.5V0.5Z" stroke="#475467"/>
+              <path d="M35 28H21M21 28L28 35M21 28L28 21" stroke="#475467" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+          </div>
+          <div class="swiper-button-next swiper-navigation arrow animation-scale-me" role="button" tabindex="0" aria-label="Next Slide">
+            <svg width="56" height="56" viewBox="0 0 56 56" fill="none" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
+              <path d="M0.5 0.5H55.5V55.5H0.5V0.5Z" stroke="#98A2B3"/>
+              <path d="M21 28H35M35 28L28 21M35 28L28 35" stroke="#475467" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </section>
 
   </div>
 <?php endif; ?>
