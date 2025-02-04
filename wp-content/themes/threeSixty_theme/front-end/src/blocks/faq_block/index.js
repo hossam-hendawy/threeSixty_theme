f392649554dffabd502e99ad2830bc4fedcc176a
dell <hendaoye.201250@gamil.com>
2025-02-04 12:56:25 +0200

    commit : faq done

--- wp-content/themes/threeSixty_theme/front-end/src/blocks/faq_block/index.js
+++ wp-content/themes/threeSixty_theme/front-end/src/blocks/faq_block/index.js
@@ -13,25 +13,33 @@ const faqBlock = async (block) => {
   // add block code here
   const accordion = block.querySelector(".accordion");
 
+// فتح أول بطاقة عند تحميل الصفحة
+  const firstPanel = accordion.querySelector(".accordion-panel");
+  if (firstPanel) {
+    toggleAccordion(firstPanel, true); // فتح أول بطاقة بشكل دائم
+  }
+
   accordion.addEventListener("click", (e) => {
     const activePanel = e.target.closest(".accordion-panel");
     if (!activePanel) return;
     toggleAccordion(activePanel);
   });
 
-  function toggleAccordion(panelToActivate) {
+  function toggleAccordion(panelToActivate, forceOpen = false) {
     const activeButton = panelToActivate.querySelector("button");
     const activePanel = panelToActivate.querySelector(".accordion-content");
     const activePanelIsOpened = activeButton.getAttribute("aria-expanded");
 
-    if (activePanelIsOpened === "true") {
-      panelToActivate.querySelector("button").setAttribute("aria-expanded", false);
-      panelToActivate.querySelector(".accordion-content").setAttribute("aria-hidden", true);
+    // إذا كان forceOpen = true نفتح البطاقة دون النظر للحالة الحالية
+    if (activePanelIsOpened === "true" && !forceOpen) {
+      activeButton.setAttribute("aria-expanded", false);
+      activePanel.setAttribute("aria-hidden", true);
     } else {
-      panelToActivate.querySelector("button").setAttribute("aria-expanded", true);
-      panelToActivate.querySelector(".accordion-content").setAttribute("aria-hidden", false);
+      activeButton.setAttribute("aria-expanded", true);
+      activePanel.setAttribute("aria-hidden", false);
     }
   }
+
 
 // testing the new hidden value
 
--- wp-content/themes/threeSixty_theme/partials/faq-card.php
+++ wp-content/themes/threeSixty_theme/partials/faq-card.php
@@ -6,13 +6,17 @@ $answer = get_field('answer', $post_id);
 <div class="accordion-panel" itemscope itemprop="mainEntity" itemtype="https://schema.org/Question">
   <?php if ($post_title) { ?>
     <div id="panel2-title" class="title">
-      <button class="accordion-trigger" aria-expanded="false" aria-controls="accordion1-content">
+      <button class="accordion-trigger medium" aria-expanded="false" aria-controls="accordion1-content">
          <span>
         <?= $post_title ?>
          </span>
-        <svg class="toggle-open minus-plus" width="24" height="26" viewBox="0 0 24 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M12 10V18M8 14H16M22 14C22 19.5228 17.5228 24 12 24C6.47715 24 2 19.5228 2 14C2 8.47715 6.47715 4 12 4C17.5228 4 22 8.47715 22 14Z" stroke="#98A2B3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
+        <div class="toggle-open minus-plus svg-border">
+          <svg xmlns="http://www.w3.org/2000/svg"  fill="#98A2B3"   width="12px" height="12px" viewBox="0 0 45.402 45.402" >
+<g>
+  <path d="M41.267,18.557H26.832V4.134C26.832,1.851,24.99,0,22.707,0c-2.283,0-4.124,1.851-4.124,4.135v14.432H4.141   c-2.283,0-4.139,1.851-4.138,4.135c-0.001,1.141,0.46,2.187,1.207,2.934c0.748,0.749,1.78,1.222,2.92,1.222h14.453V41.27   c0,1.142,0.453,2.176,1.201,2.922c0.748,0.748,1.777,1.211,2.919,1.211c2.282,0,4.129-1.851,4.129-4.133V26.857h14.435   c2.283,0,4.134-1.867,4.133-4.15C45.399,20.425,43.548,18.557,41.267,18.557z"/>
+</g>
+</svg>
+        </div>
       </button>
     </div>
   <?php } ?>
