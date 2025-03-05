2ba0cd38a779f0cdb4d4c43030cbb6c89ecc00e7
Hossam <hossam@digihive.dev>
2025-03-05 02:32:15 +0200

    feat: working in hero images

--- wp-content/themes/threeSixty_theme/blocks/blog_hero_block/index.php
+++ wp-content/themes/threeSixty_theme/blocks/blog_hero_block/index.php
@@ -31,6 +31,29 @@ $description = get_field('description');
 ?>
 <!-- region threeSixty_theme's Block -->
 <?php general_settings_for_blocks($id, $className, $dataClass); ?>
+<div class="isolation-mode">
+  <svg width="449" height="518" viewBox="0 0 449 518" fill="none">
+    <g opacity="0.8" clip-path="url(#clip0_243_7188)">
+      <path d="M149.575 518L449 517.195L298.589 258.734L149.575 518Z" fill="url(#paint0_linear_243_7188)"/>
+      <path d="M0 259.022L149.475 0H449L298.589 258.734L149.575 518L0 259.022Z" fill="url(#paint1_linear_243_7188)"/>
+    </g>
+    <defs>
+      <linearGradient id="paint0_linear_243_7188" x1="299" y1="259" x2="235.178" y2="501.181"
+                      gradientUnits="userSpaceOnUse">
+        <stop/>
+        <stop offset="1" stop-color="#3E4A5B"/>
+      </linearGradient>
+      <linearGradient id="paint1_linear_243_7188" x1="158.223" y1="-2.03715e-06" x2="150.111" y2="517.873"
+                      gradientUnits="userSpaceOnUse">
+        <stop/>
+        <stop offset="1" stop-color="#536279"/>
+      </linearGradient>
+      <clipPath id="clip0_243_7188">
+        <rect width="449" height="518" fill="white"/>
+      </clipPath>
+    </defs>
+  </svg>
+</div>
 <div class="container">
   <div class="content-wrapper flex-col">
     <?php if (function_exists('threeSixty_theme_breadcrumbs')) {
