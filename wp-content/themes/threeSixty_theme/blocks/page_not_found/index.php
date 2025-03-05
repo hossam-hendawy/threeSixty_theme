a494d231893a72d8559f45f8534d9a77dfa8e48f
Hossam <hossam@digihive.dev>
2025-03-05 23:22:00 +0200

    feat: init 404 page

---
+++ wp-content/themes/threeSixty_theme/blocks/page_not_found/index.php
@@ -0,0 +1,38 @@
+<?php
+// @author HOSSAM
+// Create id attribute allowing for custom "anchor" value.
+$id = '';
+$className = $dataClass = 'page_not_found';
+if (isset($block)) {
+  $id = 'block_' . uniqid();
+  if (!empty($block['anchor'])) {
+    $id = $block['anchor'];
+  }
+
+// Create class attribute allowing for custom "className" and "align" values.
+  if (!empty($block['className'])) {
+    $className .= ' ' . $block['className'];
+  }
+  if (!empty($block['align'])) {
+    $className .= ' align' . $block['align'];
+  }
+  if (get_field('is_screenshot')) :
+    /* Render screenshot for example */
+    echo '<img width="100%" height="100%" src="' . get_template_directory_uri() . '/blocks/page_not_found/screenshot.png" >';
+
+    return;
+  endif;
+}
+/****************************
+ *     Custom ACF Meta      *
+ ****************************/
+$title = get_field('title');
+$description = get_field('description');
+?>
+<!-- region threeSixty_theme's Block -->
+<?php general_settings_for_blocks($id, $className, $dataClass); ?>
+<div class="container">
+  <h1>Page Not Found block</h1>
+</div>
+</section>
+<!-- endregion threeSixty_theme's Block -->
