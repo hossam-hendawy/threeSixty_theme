84323400af7b0b52c94fde4650e186a651c52a21
Hossam <hossam@digihive.dev>
2025-03-13 02:09:53 +0200

    feat: map block done

---
+++ wp-content/themes/threeSixty_theme/front-end/src/blocks/map_block/index.js
@@ -0,0 +1,35 @@
+import './style.scss';
+import {imageLazyLoading} from "../../scripts/functions/imageLazyLoading";
+import {animations} from "../../scripts/general/animations";
+/**
+ * @author HOSSAM
+ * @param block {HTMLElement}
+ * @returns {Promise<void>}
+ */
+const mapBlock = async (block) => {
+
+  block.querySelectorAll('.hotspot-circle').forEach(circle => {
+    circle.addEventListener('click', function () {
+      const hiddenContent = this.querySelector('.hidden-content');
+
+      // إذا كان العنصر المفتوح هو نفسه الذي تم النقر عليه، فقم بإغلاقه فقط
+      if (hiddenContent.classList.contains('show')) {
+        hiddenContent.classList.remove('show');
+      } else {
+        // إغلاق جميع العناصر المفتوحة الأخرى
+        document.querySelectorAll('.hidden-content.show').forEach(content => {
+          content.classList.remove('show');
+        });
+
+        // فتح العنصر الجديد
+        hiddenContent.classList.add('show');
+      }
+    });
+  });
+
+    animations(block);
+    imageLazyLoading(block);
+};
+
+export default mapBlock;
+
