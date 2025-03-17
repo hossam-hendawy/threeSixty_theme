59fef8a5e28640a1de37e80cea74efb6af9fd118
Hossam <hossam@digihive.dev>
2025-03-18 00:51:16 +0200

    feat: map_block done

--- wp-content/themes/threeSixty_theme/blocks/map_block/index.php
+++ wp-content/themes/threeSixty_theme/blocks/map_block/index.php
@@ -3,6 +3,7 @@
 // Create id attribute allowing for custom "anchor" value.
 $id = '';
 $className = $dataClass = 'map_block';
+$className .= ' block-has-border ';
 if (isset($block)) {
   $id = 'block_' . uniqid();
   if (!empty($block['anchor'])) {
